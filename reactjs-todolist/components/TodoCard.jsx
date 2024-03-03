4177f7d0ca0a6828e98ceeaab9bc26935c16df31
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-03-03 12:31:16 +0530

    Refactor TodoCard and TodoList components

--- reactjs-todolist/components/TodoCard.jsx
+++ reactjs-todolist/components/TodoCard.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
-export default function TodoCard() {
+export default function TodoCard(props) {
+  const {children} = props ;
   return (
-    <div>TodoCard</div>
-  )
+    <li className="todoItem">
+      {children}
+      <div className="actionsContainer">
+        <i className="fa-regular fa-pen-to-square"></i>
+        <i className="fa-regular fa-trash-can"></i>
+      </div>
+    </li>
+  );
 }
--- reactjs-todolist/components/TodoList.jsx
+++ reactjs-todolist/components/TodoList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoCard from './TodoCard';
 
 export default function TodoList() {
     let todos = [
@@ -12,9 +13,9 @@ export default function TodoList() {
         <ul className='main'>
             {todos.map((todo, todoIndex) => {
                 return (
-                    <li className='todoItem' key={todoIndex}>{todo}
-                    <i class="fa-regular fa-pen-to-square"></i>
-                    </li>
+                    <TodoCard key={todoIndex}>
+                        <p>{todo}</p>
+                    </TodoCard>
                 );
             })}
         </ul>
